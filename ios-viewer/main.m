4fd38c43935fc19201d582a643eba991469fd26b
Citrus Circuits <citruscircuits@Citruss-MacBook-Pro.local>
2019-02-06 20:29:06 -0800

    Add UI for image gallery

--- ios-viewer/main.m
+++ ios-viewer/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  scout-viewer-2016-iOS
 //
-//  Created by Citrus Circuits on 2/8/15.
+//  Created by Citrus Circuits on 2/8 /15.
 //  Copyright (c) 2016 Citrus Circuits. All rights reserved.
 //
 
